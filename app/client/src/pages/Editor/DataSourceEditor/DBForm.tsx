e90fb4722fd59bbd363e1cc2a6c9431b3aa64faa
ohansFavour <fohanekwu@gmail.com>
2021-12-08 07:17:08 +0100

    switch auth type from form config

--- app/client/src/pages/Editor/DataSourceEditor/DBForm.tsx
+++ app/client/src/pages/Editor/DataSourceEditor/DBForm.tsx
@@ -95,7 +95,7 @@ class DatasourceDBEditor extends JSONtoForm<Props> {
   }
 
   renderDataSourceConfigForm = (sections: any) => {
-    const { datasource, messages, pluginType } = this.props;
+    const { datasource, formData, messages, pluginType } = this.props;
     const { viewMode } = this.props;
 
     return (
@@ -155,6 +155,7 @@ class DatasourceDBEditor extends JSONtoForm<Props> {
         {datasource && (
           <DatasourceAuth
             datasource={datasource}
+            formData={formData}
             getSanitizedFormData={_.memoize(this.getSanitizedData)}
             isInvalid={this.validate()}
             shouldRender={!viewMode}
